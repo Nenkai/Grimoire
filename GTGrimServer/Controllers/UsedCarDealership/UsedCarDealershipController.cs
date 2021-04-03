0e4b549174341ef3845a88fc2ffe1f3f9e92ea91
Nenkai <neenkaai@gmail.com>
2021-04-03 21:02:21 +0200

    Implement UCD controller, with untested/able XML for now

---
+++ GTGrimServer/Controllers/UsedCarDealership/UsedCarDealershipController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+using GTGrimServer.Config;
+using GTGrimServer.Utils;
+
+namespace GTGrimServer.Controllers
+{
+    /// <summary>
+    /// Provides news to the player.
+    /// </summary>
+    [ApiController]
+    [Route("/used_car/")]
+    [Produces("application/xml")]
+    public class UsedCarDealershipController : ControllerBase
+    {
+        private readonly ILogger<UsedCarDealershipController> _logger;
+        private readonly GameServerOptions _gameServerOptions;
+
+        public UsedCarDealershipController(IOptions<GameServerOptions> options, ILogger<UsedCarDealershipController> logger)
+        {
+            _logger = logger;
+            _gameServerOptions = options.Value;
+        }
+    }
+}
