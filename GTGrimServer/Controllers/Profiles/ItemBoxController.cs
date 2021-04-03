8675eed8c08f2493d7f28791775e35f268188454
Nenkai <neenkaai@gmail.com>
2021-04-03 22:32:20 +0200

    Implement/stub item box endpoint (sending items)

---
+++ GTGrimServer/Controllers/Profiles/ItemBoxController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+using GTGrimServer.Config;
+using GTGrimServer.Utils;
+using GTGrimServer.Models;
+
+namespace GTGrimServer.Controllers
+{
+    /// <summary>
+    /// Provides news to the player.
+    /// </summary>
+    [ApiController]
+    [Route("/ap/[controller]")]
+    [Produces("application/xml")]
+    public class ItemBoxController : ControllerBase
+    {
+        private readonly ILogger<ItemBoxController> _logger;
+        private readonly GameServerOptions _gameServerOptions;
+
+        public ItemBoxController(IOptions<GameServerOptions> options, ILogger<ItemBoxController> logger)
+        {
+            _logger = logger;
+            _gameServerOptions = options.Value;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            GrimRequest requestReq = await GrimRequest.Deserialize(Request.Body);
+            if (requestReq is null)
+            {
+                // Handle
+                return null;
+            }
+
+            if (requestReq.Command.Equals("itembox.getlist"))
+            {
+                return GetItemList(requestReq);
+            }
+            else
+                _logger.LogDebug($"Received unimplemented itembox command: {requestReq.Command}");
+
+            return Ok();
+        }
+
+        public ActionResult GetItemList(GrimRequest request)
+        {
+            var result = new ItemBoxList();
+            return new XmlResult(result);
+        }
+    }
+}
---
+++ GTGrimServer/Models/ItemBoxList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Xml.Serialization;
+
+namespace GTGrimServer.Models
+{
+    [XmlRoot(ElementName = "items")]
+    public class ItemBoxList
+    {
+        [XmlAttribute(AttributeName = "item")]
+        public List<ItemBox> Items { get; set; }
+    }
+}
